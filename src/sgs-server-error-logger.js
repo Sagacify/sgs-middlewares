7d4367f673c71dfe61e62e0a4cdbeb467f1a651d
Mickael van der Beek <mickael@sagacify.com>
2014-11-26 16:39:42 +0100

    Same key change as browser error logger.

--- src/sgs-server-error-logger.js
+++ src/sgs-server-error-logger.js
@@ -16,7 +16,7 @@ module.exports = (function () {
 
 	SGSServerErrorLogger.prototype.middleware = function (error, req, res, next) {
 		this.save({
-			id: req.data.id,
+			reqId: req.data.id,
 			name: error.name,
 			stack: error.stack,
 			message: error.message
